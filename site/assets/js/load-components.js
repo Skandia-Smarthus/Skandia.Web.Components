6f205864ccbf70bfcf29446625647bdae2f6aa59
Eivind Bjoraa <eivind.bjoraa@skandiaenergi.no>
2023-04-14 10:20:18 +0200

    update css

--- site/assets/js/load-components.js
+++ site/assets/js/load-components.js
@@ -2,6 +2,11 @@ window.addEventListener('DOMContentLoaded', async () => {
     const onboardingTag = document.getElementsByTagName('onboarding');
     const stromprisTag = document.getElementsByTagName('strompris');
 
+    const closestParentOnboardingTag = onboardingTag.closest('.sqs-block-content');
+    if (closestParentOnboardingTag) {
+        closestParentOnboardingTag.className = 'sqs-block-content-updated'; // set the class of the closest parent element to 'new-class'
+    }
+
     if(onboardingTag){
         for(element of onboardingTag){
             loadDynamicDocument(element, element.dataset.name, "onboarding", OnboardingSetup);
